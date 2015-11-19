de20cfc8f9e63dc30dabe915c2a601aad7e36f5a
Maxime Gil <maxime.gil.pro@gmail.com>
2015-11-19 10:58:08 +0100

    Add Database, try with Pomm

--- app/app.php
+++ app/app.php
@@ -3,14 +3,33 @@
 require_once __DIR__ . '/bootstrap.php';
 
 $app = new Silex\Application();
-$app['debug'] = true; 
+$app['debug'] = true;
 
 
 $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__ . '/../views'));
+$app->register(new PommProject\Silex\ServiceProvider\PommServiceProvider(), 
+        [
+    'pomm.configuration' =>
+    [
+        'db1' => ['dsn' => 'pgsql://user:pass@host:port/dbname'],
+       
+    ],
+    
+         'pomm.logger.service' => 'monolog', 
+        ]
+);
 
-$app->get('/', function() use ($app)  {
-	return $app['twig']->render('hello.twig.html');
- });
 
- 
- return $app; 
+
+$app->get('/', function() use ($app) {
+
+        
+    $usersdb = $app['pomm']['db1']
+    ->getQueryManager()
+    ->query('select * from users');
+    
+    return $app['twig']->render('hello.twig.html', array('users' => $usersdb) ); 
+});
+
+
+return $app;
