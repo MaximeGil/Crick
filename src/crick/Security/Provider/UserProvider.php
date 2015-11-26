4a701bd25d8509f72600b5ff72132ca5fd0327aa
Maxime Gil <maxime.gil.pro@gmail.com>
2015-11-26 16:35:18 +0100

    Modification for POMM

--- src/crick/Security/Provider/UserProvider.php
+++ src/crick/Security/Provider/UserProvider.php
@@ -1,5 +1,8 @@
 <?php
 
+namespace crick\Security\Provider;
+
+use PommProject\ModelManager\Session;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\User;
@@ -11,20 +14,21 @@ class UserProvider implements UserProviderInterface {
 
     private $conn;
 
-    public function __construct(Pomm $conn) {
+    public function __construct(Session $conn) {
         $this->conn = $conn;
+
     }
 
     public function loadUserByUsername($username) {
-        $user = $this->$conn
+
+        $user = $this->conn
                 ->getModel('db\Db\PublicSchema\UsersModel')
-                ->findWhere('name=' . $username);
+                ->findWhere('name = $*', array(strtolower($username)));
 
         if ($user->isEmpty()) {
             throw new UsernameNotFoundException(sprintf('Username "%s" does not exist.', $username));
         }
-
-        return new User($user['name'], $user['pass'], explode(',', $user['role']), true, true, true, true);
+        return new User($user->get('name'), $user->get('pass'), array($user->get('role')), true, true, true, true);
     }
 
     public function refreshUser(UserInterface $user) {
@@ -39,5 +43,4 @@ class UserProvider implements UserProviderInterface {
         return $class === 'Symfony\Component\Security\Core\User\User';
     }
 
-//put your code here
 }
