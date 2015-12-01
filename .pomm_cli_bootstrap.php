1f607f31ee048ce3dd58d58b82ce1ab4bff9d980
Maxime Gil <maxime.gil.pro@gmail.com>
2015-12-01 21:28:23 +0100

    Test 2

--- .pomm_cli_bootstrap.php
+++ .pomm_cli_bootstrap.php
@@ -8,7 +8,7 @@ $loader->add(null, __DIR__);
 
 return new Pomm(['db' =>
     [
-        'dsn' => 'hewrdngzmoezev:79xgD8rrTZzSJkHCbNQCJxrcWM@ec2-54-83-53-120.compute-1.amazonaws.com:5432/d5o0bc1t160bqg',
+        'dsn' => 'pgsql://hewrdngzmoezev:79xgD8rrTZzSJkHCbNQCJxrcWM@ec2-54-83-53-120.compute-1.amazonaws.com:5432/d5o0bc1t160bqg',
         'class:session_builder' => '\PommProject\ModelManager\SessionBuilder',
     ]
 ]);
