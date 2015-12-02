d9608c17946f2a6b47e289260d1f7bd1a2b83ac6
William Durand <will+git@drnd.me>
2015-12-02 19:01:18 +0100

    return username in controller (getPongAction)

--- src/crick/Controller/ApiController.php
+++ src/crick/Controller/ApiController.php
@@ -11,16 +11,17 @@ class ApiController
 {
     public function getPongAction(Request $request, Application $app)
     {
-        $format = $request->attributes->get('_format');
-        $user   = $app['security']->getToken()->getUser();
+        $format   = $request->attributes->get('_format');
+        $user     = $app['security']->getToken()->getUser();
+        $username = is_object($user) ? $user->getUsername() : $user;
 
         switch ($format) {
             case 'html':
-                return new Response(sprintf('<h1>pong: %s</h1>', $user));
+                return new Response(sprintf('<h1>pong: %s</h1>', $username));
             case 'json':
                 return new JsonResponse([
-                    'user'    => $user,
-                    'message' => 'pong'
+                    'username' => $username,
+                    'message'  => 'pong'
                 ]);
         }
     }
