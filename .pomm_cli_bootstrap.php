68cbf4eeb4727834d948175233d848be4146be14
Maxime Gil <maxime.gil.pro@gmail.com>
2015-12-08 18:04:44 +0100

    Add pomm bootstrap

--- .pomm_cli_bootstrap.php
+++ .pomm_cli_bootstrap.php
@@ -6,6 +6,6 @@ $loader = require __DIR__.'/vendor/autoload.php';
 $loader->add(null, __DIR__);
 
 return new Pomm(['db' => [
-    'dsn' => getenv('DATABASE_URL') ?: 'pgsql://william@localhost:5432/green',
+    'dsn' => getenv('DATABASE_URL') ?: 'pgsql://maxime:maxime@localhost:5432/green',
     'class:session_builder' => '\PommProject\ModelManager\SessionBuilder',
 ]]);
