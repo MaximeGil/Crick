dc22f1830ec240b2b0183ed7ecced5bbceeb86d7
Maxime Gil <maxime.gil.pro@gmail.com>
2015-12-08 18:40:46 +0100

    test for heroku

--- .pomm_cli_bootstrap.php
+++ .pomm_cli_bootstrap.php
@@ -5,6 +5,13 @@ use PommProject\Foundation\Pomm;
 $loader = require __DIR__.'/vendor/autoload.php';
 $loader->add(null, __DIR__);
 
+$database_url = getenv('DATABASE_URL');
+$database_url = explode("//", $database_url);
+$database_url = "pgsql://" + $database_url; 
+
+var_dump($database_url);
+die();
+
 return new Pomm(['db' => [
     'dsn' => getenv('DATABASE_URL') ?: 'pgsql://maxime:maxime@localhost:5432/green',
     'class:session_builder' => '\PommProject\ModelManager\SessionBuilder',
