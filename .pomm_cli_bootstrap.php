ed1e541d01f86c6214bf06d2ac17b26c22482e9c
Maxime Gil <maxime.gil.pro@gmail.com>
2016-01-01 14:35:47 +0100

    changes for travis

--- .pomm_cli_bootstrap.php
+++ .pomm_cli_bootstrap.php
@@ -5,11 +5,13 @@ use PommProject\Foundation\Pomm;
 $loader = require __DIR__.'/vendor/autoload.php';
 $loader->add(null, __DIR__);
 
-$database_url = getenv('DATABASE_URL');
-$database_url = explode("//", $database_url);
-$database_url = "pgsql://" . $database_url[1]; 
+// If you use Heroku
+
+//$database_url = getenv('DATABASE_URL');
+//$database_url = explode("//", $database_url);
+//database_url = "pgsql://" . $database_url[1]; 
 
 return new Pomm(['db' => [
-    'dsn' => $database_url ?: 'pgsql://maxime:maxime@localhost:5432/green',
+    'dsn' => getenv('DATABASE_URL') ?: 'pgsql://maxime:maxime@localhost:5432/green',
     'class:session_builder' => '\PommProject\ModelManager\SessionBuilder',
 ]]);
