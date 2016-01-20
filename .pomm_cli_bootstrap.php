202466e7f0124fe5ec53b14f363fbbff86133be9
MaximeGil <maxime.gil.pro@gmail.com>
2016-01-20 16:52:32 +0100

    Update .pomm_cli_bootstrap.php
    change for heroku

--- .pomm_cli_bootstrap.php
+++ .pomm_cli_bootstrap.php
@@ -7,9 +7,9 @@ $loader->add(null, __DIR__);
 
 // If you use Heroku
 
-//$database_url = getenv('DATABASE_URL');
-//$database_url = explode("//", $database_url);
-//database_url = "pgsql://" . $database_url[1]; 
+$database_url = getenv('DATABASE_URL');
+$database_url = explode("//", $database_url);
+$database_url = "pgsql://" . $database_url[1]; 
 
 return new Pomm(['db' => [
     'dsn' => getenv('DATABASE_URL') ?: 'pgsql://maxime:maxime@localhost:5432/green',
